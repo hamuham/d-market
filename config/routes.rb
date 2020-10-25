5e0ef9269b6410458e3fd5f17cf2f474053a31c2
hamuham <uni.uni.yui@hotmail.co.jp>
2020-10-25 15:50:59 +0900

    renewal

--- config/routes.rb
+++ config/routes.rb
@@ -23,6 +23,8 @@ Rails.application.routes.draw do
     end
     collection do
       get 'ready'
+      get 'category/get_category_children', to: 'items#get_category_children', defaults: { format: 'json' }
+    get 'category/get_category_grandchildren', to: 'items#get_category_grandchildren', defaults: { format: 'json' }
     end
   end
   namespace :items do
